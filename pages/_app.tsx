39d4e1d0fefb47d371aac23c54504694eaeb90fe
abozain <ahmed1998born@gmail.com>
2023-04-01 15:00:40 +0200

    mantine config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,30 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <MantineProvider withNormalizeCSS>
+            <Component {...pageProps} />
+          </MantineProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   );
 }
 
