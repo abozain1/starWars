fe094603e85e26bb65db88e9f68ae9d4bb1a33f1
abozain <ahmed1998born@gmail.com>
2023-04-01 15:15:52 +0200

    landing page styling

--- Components/Ui/FilmComp/index.tsx
+++ Components/Ui/FilmComp/index.tsx
@@ -1,6 +1,49 @@
 import { Film } from "@/Constants/films";
-import { useRouter } from "next/router";
+import { createStyles, Text, Card, rem } from "@mantine/core";
+import Link from "next/link";
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontSize: rem(34),
+    fontWeight: 900,
 
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: rem(24),
+    },
+  },
+
+  description: {
+    maxWidth: 600,
+    margin: "auto",
+
+    "&::after": {
+      content: '""',
+      display: "block",
+      backgroundColor: theme.fn.primaryColor(),
+      width: rem(45),
+      height: rem(2),
+      marginTop: theme.spacing.sm,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+
+  card: {
+    border: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  cardTitle: {
+    "&::after": {
+      content: '""',
+      display: "block",
+      backgroundColor: theme.fn.primaryColor(),
+      width: rem(45),
+      height: rem(2),
+      marginTop: theme.spacing.sm,
+    },
+  },
+}));
 export default function FilmComp({
   title,
   release_date,
@@ -8,22 +51,33 @@ export default function FilmComp({
   director,
   url,
 }: Film) {
-  const { push } = useRouter();
-  function onFilmClicked(filmUrl: string) {
-    const splitted = filmUrl.split("/");
-    const id = splitted[splitted.length - 2];
-    push({
-      pathname: "/film/[filmId]",
-      query: { filmId: id },
-    });
-  }
+  const { classes } = useStyles();
+
+  const splitted = url.split("/");
+  const id = splitted[splitted.length - 2];
+
   return (
-    <div>
-      <p style={{ color: "white" }}>{title} </p>
-      <p style={{ color: "white" }}>{release_date} </p>
-      <p style={{ color: "white" }}>{producer} </p>
-      <p style={{ color: "white" }}>{director} </p>
-      <button onClick={() => onFilmClicked(url)}>details</button>
-    </div>
+    <Link href={`film/${id}`}>
+      <Card
+        key={title}
+        shadow="md"
+        radius="md"
+        className={classes.card}
+        padding="xl"
+      >
+        <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+          {title}
+        </Text>
+        <Text fz="sm" c="dimmed" mt="sm">
+          Producer : {producer}
+        </Text>
+        <Text fz="sm" c="dimmed" mt="sm">
+          Release Date : {release_date}
+        </Text>
+        <Text fz="sm" c="dimmed" mt="sm">
+          Director : {director}
+        </Text>
+      </Card>
+    </Link>
   );
 }
